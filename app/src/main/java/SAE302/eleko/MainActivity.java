3c9e0192089b2035f46f69572c0c0420ac3b5bae
Jean-Baptiste Bruneau <jeanbaptiste.bruneau@hotmail.fr>
2023-01-09 17:41:17 +0100

    feat: Menu text is now dynamic

--- app/src/main/java/SAE302/eleko/MainActivity.java
+++ app/src/main/java/SAE302/eleko/MainActivity.java
@@ -28,7 +28,6 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(binding.getRoot());
 
-        Intent intent = getIntent();
         Jour[] data = (Jour[]) getIntent().getSerializableExtra("data"); //Get data downloaded and parsed from loadingscreen activity
 
         replaceFragment(new HomeFragment(data));
@@ -40,7 +39,60 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "No data received, app will not function correctly", Toast.LENGTH_LONG).show();
         }
 
-        Date currentTime = Calendar.getInstance().getTime(); //Get current day and time
+
+        //We need to change the menu bar to show the current day
+        //Getting today's date and putting it in java's date format
+        Date todays_date = new Date();
+        todays_date = getZeroTimeDate(todays_date);
+        Date tomorrows_date = new Date(todays_date.getTime() + (1000 * 60 * 60 * 24));
+        Date todayplustwo_date = new Date(todays_date.getTime() + (1000 * 60 * 60 * 24)*2);
+        Date todayplusthree_date = new Date(todays_date.getTime() + (1000 * 60 * 60 * 24)*3);
+
+        int i = 0;
+        for (Jour jour : data) {
+            //Checking if date is same day as today
+            if (jour.getDate().compareTo(todays_date) == 0 || jour.getDate().compareTo(tomorrows_date) == 0 || jour.getDate().compareTo(todayplustwo_date) == 0 || jour.getDate().compareTo(todayplusthree_date) == 0) {
+                //If it is, we change the menu bar to show the current day
+                //But first, we get the date of the day we want to display
+                Date date = jour.getDate();
+                //We get the day of the week
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTime(date);
+                int day = calendar.get(Calendar.DAY_OF_WEEK);
+                //We find the name of the day of the week
+                String dayName = "";
+                switch(day){
+                    case 1:
+                        dayName = ("Dimanche");
+                        break;
+                    case 2:
+                        dayName = ("Lundi");
+                        break;
+                    case 3:
+                        dayName = ("Mardi");
+                        break;
+                    case 4:
+                        dayName = ("Mercredi");
+                        break;
+                    case 5:
+                        dayName = ("Jeudi");
+                        break;
+                    case 6:
+                        dayName = ("Vendredi");
+                        break;
+                    case 7:
+                        dayName = ("Samedi");
+                        break;
+                }
+                if (binding.bottomNavigationView.getMenu().getItem(i).getTitle().equals(dayName) == true){
+                    i++;
+                }
+                else if (binding.bottomNavigationView.getMenu().getItem(i).getTitle().equals(dayName) == false){
+                    binding.bottomNavigationView.getMenu().getItem(i).setTitle(dayName);
+                }
+
+            }
+        }
 
 
 
@@ -86,5 +138,16 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frameLayout, fragment);
         fragmentTransaction.commit();
+    }
+
+    private Date getZeroTimeDate(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        date = calendar.getTime();
+        return date;
     }
 }
