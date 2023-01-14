cc7a9eacd38e550033be09b800695403708def75
Nicolas <nikolenik@hotmail.fr>
2023-01-14 15:03:20 +0100

    add database conenction

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,12 @@
 import Input.JSON_Parser;
 import Input.Web_input;
 
+import Input.Database_Import;
+import com.sun.jdi.connect.spi.Connection;
+
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.text.ParseException;
 
 
@@ -32,6 +38,19 @@ public class Main {
 
     public void choice2() {
         //TODO: add the code for the choice
+        System.Out.println("Choice 2");
+        try {
+            Connection dbConnection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/eleko", "Admin", "ii594AqcthJj6DL7");
+            system.Out.println("Connection established");
+            Database_Import db_import = new Database_Import(dbConnection);
+            system.Out.println("Database imported");
+            db_import.storeData(data_array);
+            System.out.println("Data has been successfully stored in the database");
+        } catch (SQLException e) {
+            System.Out.println("Connection failed");
+            e.printStackTrace();
+        }
     }
 
 }
+
